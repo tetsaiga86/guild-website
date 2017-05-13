1853035984c78b6fc4613349b09c323fb0f1ac8a
kyle <tetsaiga86@gmail.com>
2017-05-12 20:54:33 -0700

    achievement.jsx test

--- __tests__/achievement.test.js
+++ __tests__/achievement.test.js
@@ -1,6 +1,7 @@
 import Achievement from '../client/components/achievement.jsx'
 import React from 'react'
 import {shallow} from 'enzyme'
+import sinon from 'sinon'
 import {
   Table,
   Popover,
@@ -8,10 +9,35 @@ import {
 } from 'react-bootstrap'
 import achievements from '../spec/fixtures/achievements.json'
 
+const iconUrl = 'http://media.blizzard.com/wow/icons/56/';
+const achievementUrl = 'http://www.wowhead.com/achievement=';
+const achievement1=achievements.achievements[0]
+const details = achievement1.details;
+
+const component = shallow(
+  <Achievement achievement={achievement1} />
+)
+const overlayTrigger = component.find(OverlayTrigger)
+const overlay = overlayTrigger.prop('overlay')
+const renderedOverlay = shallow(overlay)
+const td = overlayTrigger.find('td')
+
 test('renders an OverlayTrigger', ()=>{
-  const achievement1=achievements.achievements[0]
-  const component = shallow(
-    <Achievement achievement={achievement1} />
-  )
   expect(component.find(OverlayTrigger).length).toBe(1)
+})
+
+test('gives the correct number of rendered criteria inside overlay prop of OverlayTrigger component', ()=>{
+  expect(renderedOverlay.find('li').length).toBe(achievement1.details.criteria.length)
+})
+
+test('.achievement-table-cell has onClick listener that invokes window.open with correct url', ()=>{
+  var criteria = details.criteria;
+  var stub = sinon.stub(window, 'open')
+  overlayTrigger.children('.achievement-table-cell').simulate('click');
+  expect(stub.calledWith(`${achievementUrl}${achievement1.id}/${details.title}`))
+  stub.restore()
+})
+
+test('renders img with correct src', ()=>{
+  expect(td.children('.achievement-icon').prop('src')).toBe(`${iconUrl}${details.icon}.jpg`)
 })
---
+++ __tests__/achievements.test.js
@@ -0,0 +1,15 @@
+import Achievements from '../client/components/achievements.jsx'
+import React from 'react'
+import {shallow} from 'enzyme'
+import {
+  Table,
+
+} from 'react-bootstrap'
+import achievements from '../spec/fixtures/achievements.json'
+
+test('renders Achievements', ()=>{
+  // const component = shallow(
+  //   <Achievements />
+  // )
+  // expect(component.find(Achievements)).toBe(true)
+})
